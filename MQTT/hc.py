e6cb4295ba8d7fdda127d2ab624a9d671bff001f
JhosephL <jhosephslizarazom@outlook.com>
2023-11-21 04:22:15 -0500

    Termino variable de tiempo de mensaje y es tolerante a fallos.

--- MQTT/hc.py
+++ MQTT/hc.py
@@ -2,6 +2,8 @@
 # IMPORTS
 ## Parametros por consola y salida de programa.
 import sys
+## Tiempo
+from datetime import datetime
 ## Protocolo de comunicación.
 import paho.mqtt.client as mqtt
 
@@ -44,7 +46,7 @@ def on_message ( client , userdata , message ) :
 
     mensajeDeLlegada = message.payload.decode()
     
-    if ( mensajeDeLlegada == "T" or mensajeDeLlegada == "pH" or mensajeDeLlegada == "OD" ) :
+    if ( mensajeDeLlegada == "T" or mensajeDeLlegada == "pH" or mensajeDeLlegada == "OD" or mensajeDeLlegada == "Tiempo" ) :
 
         global TOOPICO
         TOOPICO = mensajeDeLlegada
@@ -86,6 +88,23 @@ def funcioon_monitorear ( mensajeDeLlegada , mensajeDeRecibir ) :
             function_writeFile ( "log" , mensajeDeRecibir )
         else :
             client.publish ( "Alarmas" , mensajeDeRecibir )
+
+    elif ( TOOPICO == "Tiempo" ) :
+
+        # Tiempo menor.
+        int_mensajeDeLlegada = int ( mensajeDeLlegada )
+
+        # Tiempo mayor.
+        tiempo = datetime.now()
+        minutos = tiempo.minute
+        segundos = tiempo.second
+        milisegundos = tiempo.microsecond // 1000
+        total_milisegundos = ( minutos*60*1000 ) + ( segundos*1000 ) + milisegundos
+
+        # TM - Tm
+        diferencia = total_milisegundos - int_mensajeDeLlegada
+
+        print ( "Tiempo: " + str(diferencia) )
 
 
 def function_createFile ( string_fileName ) :
