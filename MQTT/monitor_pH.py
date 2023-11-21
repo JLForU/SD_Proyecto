06df126cdad1e8fb2289a291ff0491497827d22c
JhosephL <jhosephslizarazom@outlook.com>
2023-11-20 22:59:37 -0500

    Base de datos lista.

--- MQTT/monitor_pH.py
+++ MQTT/monitor_pH.py
@@ -7,7 +7,7 @@ import paho.mqtt.client as mqtt
 # VARIABLES GLOBALES
 # Establecer conexión.
 client = mqtt.Client()
-client.connect("localhost", 1883)
+client.connect ( "localhost" , 1883 )
 ## Tópico
 TOOPICO = "pH"
 
@@ -17,6 +17,9 @@ def main ( ) :
 
     print ("\n\n")
 
+
+    # Crear archivo BD.
+    function_createFile ( "log" )
 
     # Recibir mensaje, especificando un tópico.
     client.subscribe ( TOOPICO )
@@ -37,8 +40,54 @@ def main ( ) :
 
 
 # OTHER FUNCTIONS
-def on_message(client, userdata, message):
-    print(f"Recibir: {message.payload.decode()}; por medio de '{message.topic}'.")
+def on_message ( client , userdata , message ) :
+
+    mensaje = f"Recibir: {message.payload.decode()}; por medio de '{message.topic}'.\n"
+    function_writeFile ( "log" , mensaje )
+    print ( mensaje )
+
+
+def function_createFile ( string_fileName ) :
+
+    print ( "FILE CREATION\n" )
+
+    try :
+
+        inStreamFile_createFile = open ( string_fileName , "x" )
+        inStreamFile_createFile.close()
+        print ( f"\tThe file '{string_fileName}' was successfully created." )
+
+    except FileExistsError :
+
+        print ( f"\tThe file '{string_fileName}' already exists." )
+
+    except Exception as e :
+
+        print ( f"\tAn error occurred while creating the file: {e}." )
+
+    print ( '\n' )
+
+
+def function_writeFile ( string_fileName , string_fileContent ) :
+
+    print ( "FILE WRITING\n" )
+
+    try :
+
+        outStreamFile_writeFile = open ( string_fileName , "a" )
+        outStreamFile_writeFile.write ( string_fileContent )
+        outStreamFile_writeFile.close()
+        print ( f"\tThe file '{string_fileName}' was successfully written." )
+
+    except FileNotFoundError :
+
+        print ( f"\tThe file '{string_fileName}' does not exist." )
+
+    except Exception as e :
+
+        print ( f"\tAn error occurred while writing the file: {e}." )
+
+    print ( '\n' )
 
 
 
